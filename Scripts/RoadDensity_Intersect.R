96e949d63ff2e5b5f9b670caf80dda49345c8db2
SarahBassing <sarah.bassing@gmail.com>
2020-12-02 14:05:19 -0800

    new

--- Scripts/RoadDensity_Intersect.R
+++ Scripts/RoadDensity_Intersect.R
@@ -20,22 +20,22 @@ library(rgeos)
 # First a toy example with roads data from Tanzania
 # Link to Tanzania roads file: http://www.diva-gis.org/gdata
 
-roads <- shapefile("TZA_roads.shp")
-roads <- spTransform(roads, CRS("+proj=utm +zone=37 +south +datum=WGS84"))
-rs <- raster(extent(roads), crs=projection(roads))
-rs[] <- 1:ncell(rs)
-
-# Intersect lines with raster "polygons" and add length to new lines segments
-rsp <- rasterToPolygons(rs)
-rp <- intersect(roads, rsp)
-rp$length <- gLength(rp, byid=TRUE) / 1000 # to convert m to km
-x <- tapply(rp$length, rp$layer, sum)
-r <- raster(rs)
-r[as.integer(names(x))] <- x
-
-# Visualize Results
-plot(r)
-plot(roads, add = T)
+# roads <- shapefile("TZA_roads.shp")
+# roads <- spTransform(roads, CRS("+proj=utm +zone=37 +south +datum=WGS84"))
+# rs <- raster(extent(roads), crs=projection(roads))
+# rs[] <- 1:ncell(rs)
+# 
+# # Intersect lines with raster "polygons" and add length to new lines segments
+# rsp <- rasterToPolygons(rs)
+# rp <- intersect(roads, rsp)
+# rp$length <- gLength(rp, byid=TRUE) / 1000 # to convert m to km
+# x <- tapply(rp$length, rp$layer, sum)
+# r <- raster(rs)
+# r[as.integer(names(x))] <- x
+# 
+# # Visualize Results
+# plot(r)
+# plot(roads, add = T)
 
 ############################################################################
 
@@ -43,32 +43,25 @@ plot(roads, add = T)
 
 #load shapefiles and grid raster
 roads <- shapefile("WPPProads_Cascades.shp")
-NE <- shapefile("NE_covariate_area.shp")
-WPPP <- shapefile("WPPP_CovariateBoundary.shp")
-grid <- raster("ref_grid_4k.img")
+NE <- shapefile("NE_covariate_area_2855.shp")
 grid_NE <- raster("NE_grid_4k.img")
 
 #check projection
 crs(roads)
 crs(NE)
-crs(WPPP)
-crs(grid)
 crs(grid_NE)
 
 # project needed files to EPSG 2855 and check
 roads <- spTransform(roads, CRS("+init=epsg:2855"))
-NE <- spTransform(NE, CRS("+init=epsg:2855"))
-WPPP <- spTransform(WPPP, CRS("+init=epsg:2855"))
+crs(roads)
 
 # visualize the data
-plot(grid)
+plot(grid_NE)
 plot(roads)
-plot(WPPP, add=T)
 plot(NE, add=T)
 
-
 # crop full roads layer to grid boundary (since they are off otherwise)
-roads_NE <- crop(roads, grid_NE)
+roads_NE <- crop(roads, NE)
 plot(grid_NE)
 plot(roads_NE, add=T)
 plot(NE, add=T)
@@ -76,8 +69,6 @@ plot(NE, add=T)
 # create empty cells within the grid
 grid_NE[] <- 1:ncell(grid_NE)
 grid_NE
-plot(grid_NE)
-plot(roads_NE, add=T)
 
 # Intersect lines with raster "polygons" and add length to new lines segments
 grid_NEp <- rasterToPolygons(grid_NE)
@@ -88,9 +79,14 @@ x <- tapply(rp$length, rp$NE_grid_4k, sum)
 r <- raster(grid_NE)
 r[as.integer(names(x))] <- x
 
+# replace NA values with zero (NAs are occuring in cell blocks with no roads)
+r[is.na(r[])] <- 0
+
 # Visualize Results
 plot(r)
 plot(roads_NE, add = T)
+plot(NE, add=T)
+summary(r)
 writeRaster(r,"NE_RoadDensity_4k.tif")
 
 ####################################################
